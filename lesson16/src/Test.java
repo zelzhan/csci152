343ba084a3a7ac5a7ee8af19f0a87b9164ff876f
elzhan <elzhan.zeinulla@nu.edu.kz>
2018-03-13 23:49:48 +0600

    hello

--- lesson16/src/Test.java
+++ lesson16/src/Test.java
@@ -4,28 +4,52 @@ public class Test {
     public static void main(String[] args){
         Set<Integer> bst = new BSTSet<>();
         bst.add(5);
-        bst.add(12);
-        bst.add(3);
-        bst.add(6);
-        bst.add(14);
-        bst.add(2);
-        bst.add(-6);
-        bst.add(8);
-        bst.add(0);
-        bst.add(2);
+        bst.add(30);
+        bst.add(320);
+        bst.add(3340);
+        bst.add(340);
+//        bst.add(5);
+//        bst.add(12);
+//        bst.add(3);
+//        bst.add(6);
+//        bst.add(14);
+//        bst.add(2);
+//        bst.add(-6);
+//        bst.add(8);
+//        bst.add(0);
+//        System.out.println(bst);
+//        bst.add(14);
+//        bst.add(-6);
+//        bst.add(5);
         System.out.println(bst);
-        bst.add(14);
-        bst.add(-6);
-        bst.add(5);
-        System.out.println(bst);
-        System.out.println(bst.contains(12));
-        System.out.println(bst.contains(7));
-        System.out.println(bst.contains(2));
-        System.out.println(bst.contains(-8));
-        System.out.println(bst.contains(5));
-        System.out.println("Size " + bst.getSize());
-        bst.clear();
-        System.out.println(bst);
-
+//        System.out.println(bst.contains(12));
+//        System.out.println(bst.contains(7));
+//        System.out.println(bst.contains(2));
+//        System.out.println(bst.contains(-8));
+//        System.out.println(bst.contains(5));
+//        System.out.println("Size " + bst.getSize());
+//        bst.remove(0);
+//        bst.remove(6);
+//        bst.remove(3);
+//        System.out.println(bst);
+//        bst.add(6);
+//        bst.add(3);
+//        bst.add(99);
+//        bst.add(11);
+//        System.out.println(bst);
+//        bst.remove(12);
+//        System.out.println(bst);
+//        bst.remove(5);
+//        System.out.println(bst);
+//        try{
+//            bst.removeAny();
+//            bst.removeAny();
+//            bst.removeAny();
+//
+//        } catch (Exception ex){
+//
+//        }
+//        System.out.println(bst.getSize());
+//        System.out.println(bst);
     }
 }
--- lesson16/src/impl/BSTSet.java
+++ lesson16/src/impl/BSTSet.java
@@ -8,6 +8,7 @@
 
 package impl;
 import adt.Set;
+import sun.reflect.generics.tree.Tree;
 
 public class BSTSet<T extends Comparable> implements Set<T> {
     private TreeNode<T> root;
@@ -42,12 +43,87 @@ public class BSTSet<T extends Comparable> implements Set<T> {
 
     @Override
     public boolean remove(T value) {
-        return false;
+        if (!contains(value)) return false;
+        size--;
+        TreeNode<T> link = root;
+        TreeNode<T> prevLink = link;
+        while(!link.getValue().equals(value)){
+            if(link.getValue().compareTo(value) < 0){
+                prevLink = link;
+                link = link.getRight();
+            }
+            if(link.getValue().compareTo(value) > 0){
+                prevLink = link;
+                link = link.getLeft();
+            }
+        }
+        //case 1
+        if(link.getRight()==null && link.getLeft() == null){
+            if(prevLink.getValue().compareTo(value) < 0) prevLink.setRight(null);
+            else prevLink.setLeft(null);
+            return true;
+        }
+
+        //case 2
+        if(link.getLeft() == null){
+            if(prevLink.getValue().compareTo(value) < 0) prevLink.setRight(link.getRight());
+            else prevLink.setLeft(link.getRight());
+            return true;
+        }
+
+        if(link.getRight() == null){
+            if(prevLink.getValue().compareTo(value) < 0) prevLink.setRight(link.getLeft());
+            else prevLink.setLeft(link.getLeft());
+            return true;
+        }
+
+        //case 3
+        TreeNode<T> temp = link.getRight();
+        TreeNode<T> temp2 = link;
+
+        //root case
+        if(link == prevLink){
+            while(temp.getLeft() != null){
+                temp2 = temp;
+                temp = temp.getLeft();
+            }
+            temp2.setLeft(null);
+            temp.setLeft(root.getLeft());
+            temp.setRight(root.getRight());
+            root = temp;
+            return true;
+        };
+
+        //general case
+        if(temp.getLeft() == null){
+            temp.setLeft(link.getLeft());
+            if(prevLink.getValue().compareTo(temp.getValue()) < 0) prevLink.setRight(temp);
+            else prevLink.setLeft(temp);
+            return true;
+        }
+        while(temp.getLeft() != null){
+            temp2 = temp;
+            temp = temp.getLeft();
+        }
+        temp2.setLeft(null);
+        temp.setLeft(link.getLeft());
+        temp.setRight(link.getRight());
+        if(prevLink.getValue().compareTo(temp.getValue()) < 0) prevLink.setRight(temp);
+        else prevLink.setLeft(temp);
+
+        return true;
     }
 
     @Override
     public T removeAny() throws Exception {
-        return null;
+//        if(size == 0) throw new Exception("Exception");
+//        TreeNode<T> node = root;
+//        while(node != null && size != 1) node = node.getLeft();
+
+        if(size == 0) throw new Exception("Empty!");
+        T val = root.getValue();
+        remove(root.getLeft().getValue());
+        return val;
     }
 
     @Override
@@ -92,6 +168,16 @@ public class BSTSet<T extends Comparable> implements Set<T> {
             return containsHelper(node.getLeft(), value);
         }
         return false;
+    }
+
+    private TreeNode removeHelper(TreeNode<T> node, T value){
+        if(node.getValue().compareTo(value) == 0) return node;
+        if(node.getValue().compareTo(value) < 0){
+            node = addHelper(node.getRight(), value);
+        } else if(node.getValue().compareTo(value) > 0){
+            node = addHelper(node.getLeft(), value);
+        }
+        return node;
     }
 
 
